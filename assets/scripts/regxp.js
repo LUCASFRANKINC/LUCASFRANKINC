e87203fb8621838a412c9305b2acefa24a865993
Frank <lucasfrank254@gmail.com>
2022-07-05 14:27:54 +0300

    Update regxp

--- assets/scripts/regxp.js
+++ assets/scripts/regxp.js
@@ -1,4 +1,4 @@
 let string = "Lucas Francis, 2022, Copyright.";
 let regex = /(\w+)\s(\w+)(,)\s\d+\3\s\w+/; //using \n => where n is a positive integer for back referencing.
 let match = regex.exec(string);
-console.log(`${match[2]}, ${match[1]}`)
+console.log(`${match[2]}, ${match[1]}`); //using capturing groups. Items are stored as an array.
