3d9af3b9563fb06e4d4383a90b6a805f99679b77
Frank <lucasfrank254@gmail.com>
2022-07-05 14:26:47 +0300

    Just started.

--- assets/scripts/regxp.js
+++ assets/scripts/regxp.js
@@ -1,1 +1,4 @@
-console.log('The endlessly opportunity ever!'.match(/\bendless|ever\b/));
+let string = "Lucas Francis, 2022, Copyright.";
+let regex = /(\w+)\s(\w+)(,)\s\d+\3\s\w+/; //using \n => where n is a positive integer for back referencing.
+let match = regex.exec(string);
+console.log(`${match[2]}, ${match[1]}`)
