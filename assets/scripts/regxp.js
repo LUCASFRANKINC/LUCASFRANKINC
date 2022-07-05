a0a98177428cab10e06a25da5e0c21c311569188
Frank <lucasfrank254@gmail.com>
2022-07-05 21:52:24 +0300

    regxp update, commented

--- assets/scripts/regxp.js
+++ assets/scripts/regxp.js
@@ -1,4 +1,16 @@
+
+//**Capturing groups and back referencing***************************
 let string = "Lucas Francis, 2022, Copyright.";
 let regex = /(\w+)\s(\w+)(,)\s\d+\3\s\w+/; //using \n => where n is a positive integer for back referencing.
 let match = regex.exec(string);
 console.log(`${match[2]}, ${match[1]}`); //using capturing groups. Items are stored as an array.
+//******End of back capturing groups and back referencing*************
+
+//****** Named capturing group and its back reference*****************
+
+let namedCaptureGroupRegExp = /(?<title>\w+),\soh\s\k<title>/i; //Named capture group and a back reference. ie. (?<title>\w+) referenced by \k<title>
+let str = "Lord, Oh lord!"
+let executed = namedCaptureGroupRegExp.exec(str);
+console.log(str.match(namedCaptureGroupRegExp))
+
+//*******End of Named capturing group and its back reference************
