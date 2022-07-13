1ba260ada2db65baceaa533b566ce51e616906a7
Frank <lucasfrank254@gmail.com>
2022-07-13 09:24:27 +0300

    Indices redone in match.js

--- assets/scripts/chapter_RegularExpressions/methods/match.js
+++ assets/scripts/chapter_RegularExpressions/methods/match.js
@@ -1,5 +1,24 @@
-let string = "The quick brown fox, yeah, the only quick brown damn fox, jumped over the lazy dog";
-let regXp = /quick\s(brown).*?(?<animal>fox)/d;
-//regXp.lastIndex = 36;
+let string =
+  "The quick brown fox, yeah, the only quick brown dog damn fox, jumped over the lazy dog";
+let regXp = /quick\s(brown).*?(?<animal>dog|fox),(?<action>.*)\Wover/dy;
+regXp.lastIndex = 36;
 let matched = string.match(regXp);
-console.log(matched)
+console.table({
+  Matched: matched[0],
+  "Captured groups": [matched[1], matched[2]],
+  "Specified match index": matched.index,
+  "Indices and findings": (function (x) {
+    let arr = Array.of();
+    for (let i of x.indices) arr.push(i);
+    arr.push({
+      "Indices count": x.indices.length,
+      "Indices Groups": x.indices.group,
+    });
+    return arr;
+  })(matched),
+  Groups: (function (x) {
+    let arr = [];
+    for (let key in x.groups) arr.push({ [key]: x.groups[key] });
+    return arr;
+  })(matched),
+});
