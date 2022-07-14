5ca1c65b09075e4d9e39345f08c7420e71b5b88e
Frank <lucasfrank254@gmail.com>
2022-07-14 12:52:43 +0300

    comprehensive replace

--- assets/scripts/chapter_RegularExpressions/methods/replace.js
+++ assets/scripts/chapter_RegularExpressions/methods/replace.js
@@ -4,6 +4,10 @@
 let string =
   "The quick brown fox jumped over the lazy dog. The yellow fox was intriguingly fast";
 let regXp = new RegExp(/(?<color>brown|yellow)?(\s)(fox)/, "ig");
+
+/**
+ * Specify a string as a parameter
+ */
 //--------------------Using special characters ('$x') as replacement strings--------------------
 
 //using named capturing groups
@@ -31,3 +35,63 @@ console.table({
   "Inserting string after match => ": insertPostString,
   "Using nth parenthesized substring => ": replaceNth,
 });
+
+/**
+ * Specifying a function as a parameter
+ * The function's return value is used as the replacement string.
+ */
+//Function arguments.
+//match => This is the matched substring. Same as '$&' above.
+//p1, p2, ... => The nth parenthesized capturing group, inclusive of named capturing groups.
+//offset => The offset of the matched substring in relation to the string. eg. if string was 'abcd' and the match was 'cd', offset = 2.
+//string => The whole string being examined
+//groups => named capturing groups, where key are the group names.
+
+let str = "abcd12345/*-+%";
+
+/**
+ * **NB: The args above have to be arranged in that order**
+ */
+const replacerFunc = (match, p1, p2, p3, offset, string, groups) => {
+  return [
+    p1,
+    p2,
+    p3,
+    ((x) => {
+      let str = "\n";
+      for (let i in x) {
+        str += i + ":" + x[i] + "\n";
+      }
+      return str;
+    })(groups),
+  ].join("-");
+};
+
+let newStr = str.replace(
+  /(?<name>[^\d]+?)(?<number>\d+?)([^\w\s]+)/i,
+  replacerFunc
+);
+console.log(newStr);
+
+//Switch names in a string
+let s = "Shine your way, Kobe Bryant";
+let reg = new RegExp(/(?<firstname>Kobe)\s(?<lastname>[\w]+)/, "gi");
+let switched = s.replace(reg, "$2 $1");
+console.log(switched);
+
+//change string functions camelCase to camel-case.eg. borderTop to border-top
+function changeCase(x) {
+    if(x) {
+        let regXp = /[A-Z]/g;
+        let newStr = '';
+        if(regXp.test(x)) {
+            newStr = x.replace(regXp,function(match,offset, string){
+                //check offset to see if it appears at string beginning
+                return (offset > 0 ? '-' : '') + match.toLowerCase();
+            });
+        }
+        else newStr = "Your string does not seem to have the camelCase to be changed to camel-case!";
+        return newStr;
+    }
+}
+console.log(changeCase('FontWeight'));//logs font-weight
