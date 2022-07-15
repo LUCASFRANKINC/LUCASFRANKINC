8aadccb2f7dfa53c7a18cd8a6530efcbd3b48bc5
Frank <lucasfrank254@gmail.com>
2022-07-15 17:38:08 +0300

    split

---
+++ assets/scripts/chapter_RegularExpressions/methods/split.js
@@ -0,0 +1,20 @@
+//Divides a string into an ordered list of substrings, puts them into an array and returns it.
+//split(separator, limit(*optional*))
+
+//split empty string. Returns an array with one element => an empty string
+let string = "";
+console.log(string.split());
+
+//function to split a string with a specified separator
+function splitString(str, sep) {
+    const separated = str.split(sep);
+    console.table({"Original string: ": str, "Separator: ": sep, "Output: ": separated});
+}
+
+splitString("The old shit: yeah: it's damn : doom", ":");
+
+//use a regular expression to split. Plus a limit of 3 names only.
+let names = "Harry Trump ;Fred Barney; Helen Rigby ; Bill Abel ;Chris Hand & Cynthia Ruthrock";
+let regXp = /\s*(?:;|&)\s*/g;
+let sep = names.split(regXp, 3);
+console.log(sep);
