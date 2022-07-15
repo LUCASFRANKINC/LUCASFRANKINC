d4690e70e2096d09f423d3bc982e0bf33a596747
Frank <lucasfrank254@gmail.com>
2022-07-15 18:20:11 +0300

    Mastery complete : split

--- assets/scripts/chapter_RegularExpressions/methods/split.js
+++ assets/scripts/chapter_RegularExpressions/methods/split.js
@@ -14,7 +14,14 @@ function splitString(str, sep) {
 splitString("The old shit: yeah: it's damn : doom", ":");
 
 //use a regular expression to split. Plus a limit of 3 names only.
+//NB: Used non-capturing parentheses. Else, the matches would have been include
+//in the array
 let names = "Harry Trump ;Fred Barney; Helen Rigby ; Bill Abel ;Chris Hand & Cynthia Ruthrock";
 let regXp = /\s*(?:;|&)\s*/g;
 let sep = names.split(regXp, 3);
 console.log(sep);
+
+//Illustrating use of capturing parentheses
+let str = "Get 3 avocadoes and 14 apples";
+let regWithCaptParentheses = new RegExp(/((?:\d+)\s(?:\w+))(?:\s?)/);
+console.log(str.split(regWithCaptParentheses));
